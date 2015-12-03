e149769b625166a3997baf3b4f29b2c616a65f79
Josef Reidinger <jreidinger@suse.cz>
2015-12-03 16:01:55 +0100

    add model for device map file

---
+++ lib/cfa/grub2/device_map.rb
@@ -0,0 +1,55 @@
+require "cfa/base_model"
+require "cfa/augeas_parser"
+require "cfa/placer"
+require "cfa/matcher"
+
+module CFA
+  module Grub2
+    # Represents grub device map in /etc/grub2/device_map
+    # for details see https://www.gnu.org/software/grub/manual/html_node/Device-map.html
+    # Main features:
+    #
+    # - Do not overwrite files
+    # - When setting value first try to just change value if key already exists
+    # - When grub key is not there, then add to file
+    class DeviceMap < BaseModel
+      PARSER = AugeasParser.new("device_map.lns")
+      PATH = "/etc/grub2/device.map"
+
+      def initialize(file_handler: File)
+        super(PARSER, PATH, file_handler: file_handler)
+        # TODO: add to parser method to fill empty data tree
+        self.data = AugeasTree.new
+      end
+
+      def grub_device_for(system_dev)
+        # TODO: maybe move to generic tree find key for value?
+        matcher = Matcher.new(value_matcher: system_dev)
+        entry = data.select(matcher)
+
+        entry.empty? ? nil : entry.first[:key]
+      end
+
+      def system_device_for(grub_device)
+        data[grub_device]
+      end
+
+      def add_mapping(grub_device, system_device)
+        generic_set(grub_device, system_device)
+      end
+
+      def remove_mapping(grub_device)
+        data.delete(grub_device)
+      end
+
+      def grub_devices
+        # TODO: maybe add matcher which allow regexp for key or negative regexp
+        entries = data.data.select do |entry|
+          entry[:key] !~ /comment/
+        end
+
+        entries.map { |e| e[:key] }
+      end
+    end
+  end
+end
